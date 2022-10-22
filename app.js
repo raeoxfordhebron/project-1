7e5ba5f6d53732252c6e139ba892943638178872
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-10-22 15:09:41 -0400

    added more data onto DOM

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const apiKey = "ff13e44c-13f5-4d10-ae1a-09f957918836"
 // variable for base url
 const baseURL = "https://api.nookipedia.com/"
 
-// save some variables
+// save some HTML variables
 const $title = $("#title")
 const $main = $("main")
 const $villagerTextName = $("#villagerTextName")
 const $villagerName = $("#villagerName")
 const $quote = $("#quote")
 const $birthdayMonth = $("#birthdayMonth")
+const $image = $("#image")
+const $clothing = $("#clothing")
+const $species = $("#species")
+const $sign = $("sign")
 
 
 // function that does villager search
@@ -24,13 +28,15 @@ function villagerSearch(name) {
         // $main.empty()
         // render data onto DOM
         $villagerName.text(`Villager Name: ${data[0].name}`)
-        $birthdayMonth.html(`Birthday Month: ${data[0].birthday_month}`)
-        $quote.html(`"${data[0].quote}"`)
+        $birthdayMonth.text(`Birthday Month: ${data[0].birthday_month}`)
+        $quote.text(`"${data[0].quote}"`)
+        $image.html(`<img src="${data[0].image_url}">`)
+        $clothing.text(`Clothing Choice: ${data[0].clothing}`)
+        $species.text(`Species: ${data[0].species}`)
+        $sign.text(`Astrological Sign: ${data[0].sign}`)
     })
 }
 
-// function render() {
-//     $villagerName.text(villager[0].name)
-// }
+
 
 villagerSearch("Bob")
