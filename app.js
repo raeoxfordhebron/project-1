883cb3a2494626d297d473c02bec1af9223a8166
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-10-22 14:15:44 -0400

    rendered some data to DOM in less perferable way

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const apiKey = "ff13e44c-13f5-4d10-ae1a-09f957918836"
 const baseURL = "https://api.nookipedia.com/"
 
 // save some variables
+const $title = $("#title")
 const $main = $("main")
+const $villagerTextName = $("#villagerTextName")
+const $villagerName = $("#villagerName")
+const $quote = $("#quote")
+const $birthdayMonth = $("#birthdayMonth")
+
 
 // function that does villager search
 function villagerSearch(name) {
@@ -13,12 +19,18 @@ function villagerSearch(name) {
         url: `${baseURL}villagers?name=${name}`,
         headers: {"X-API-KEY": `${apiKey}`}
     })
-    .then((villager) => {
-        console.log(villager)
-        $main.empty()
+    .then((data) => {
+        console.log(data)
+        // $main.empty()
         // render data onto DOM
-        $main.html(`<h2>${villager[0].name}`)
+        $villagerName.text(`Villager Name: ${data[0].name}`)
+        $birthdayMonth.html(`Birthday Month: ${data[0].birthday_month}`)
+        $quote.html(`"${data[0].quote}"`)
     })
 }
+
+// function render() {
+//     $villagerName.text(villager[0].name)
+// }
 
 villagerSearch("Bob")
