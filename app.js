85231e2d686b7a1b33306ce02318c6cac3492aeb
Rae Hebron <rae.oxford.hebron@gmail.com>
2022-10-22 11:35:11 -0400

    functional promise

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ const apiKey = "ff13e44c-13f5-4d10-ae1a-09f957918836"
 
 // variable for base url
 const baseURL = "https://api.nookipedia.com/"
+
+// save some variables
+const $main = $("main")
+
+// function that does villager search
+function villagerSearch(name) {
+    $.ajax({
+        url: `${baseURL}villagers?name=${name}`,
+        headers: {"X-API-KEY": `${apiKey}`}
+    })
+    .then((villager) => {
+        console.log(villager)
+        $main.empty()
+        // render data onto DOM
+        $main.html(`<h2>${villager[0].name}`)
+    })
+}
+
+villagerSearch("Bob")
